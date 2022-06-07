8429e468750f15bb052b4f75f8bbd8ce750d084c
karabink <48476624+karabink@users.noreply.github.com>
2022-06-07 23:19:32 +0300

    Open

--- src/components/contacts/index.vue
+++ src/components/contacts/index.vue
@@ -1,15 +1,26 @@
 <template>
- <div class="contacts">
-     <div class="_2egft">
-         <div id="Containerc24vq" class="_1Cfot">
-             <div data-mesh-id="Containerc24vqinlineContent" data-testid="inline-content" class="">
-                 <div data-mesh-id="Containerc24vqinlineContent-gridContainer" data-testid="mesh-container-content">
-                     <div id="WRchTxt1d-c8m" class="_2bafp" data-testid="richTextElement">
-                         <h1 class="font_0" style="text-align:center;font-size:25px">
-                         <span>Контакти</span>
-                         </h1>
-                     </div>
-                         <!--
+  <div class="contacts">
+    <div class="_2egft">
+      <div id="Containerc24vq" class="_1Cfot">
+        <div
+          data-mesh-id="Containerc24vqinlineContent"
+          data-testid="inline-content"
+          class=""
+        >
+          <div
+            data-mesh-id="Containerc24vqinlineContent-gridContainer"
+            data-testid="mesh-container-content"
+          >
+            <div
+              id="WRchTxt1d-c8m"
+              class="_2bafp"
+              data-testid="richTextElement"
+            >
+              <h1 class="font_0" style="text-align: center; font-size: 25px">
+                <span>Контакти</span>
+              </h1>
+            </div>
+            <!--
                          <div id="mapContainer" class="mC">
                           <iframe title="Карти Google" 
                            aria-label="Карти Google" data-src="" width="100%" height="100%" frameborder="0" 
@@ -17,92 +28,179 @@
                           </iframe>
                          </div>
                          -->
-                         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d9179.613521464089!2d22.32552021134186!3d48.5254220232701!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47391cf77e74bd6d%3A0x73bb9b59acf39158!2z0JTQvtC70LPQvtC1INCf0L7Qu9C1LCDQl9Cw0LrQsNGA0L_QsNGC0YHQutCw0Y8g0L7QsdC70LDRgdGC0YwsIDg5NDM0!5e1!3m2!1sru!2sua!4v1654112676036!5m2!1sru!2sua" 
-                         max-width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade">
-                         </iframe>
-                     <div id="Info" class="font_6" data-testid="richTextElement">
-                         <p class="font_7" style="font-size:17px">
-                         <span class="color_34">Phone1: +380503376463</span>
-                         </p>
-                         <p class="font_7" style="font-size:17px">
-                         <span class="color_34">Phone2: +380678891187</span>
-                         </p>
-                         <p class="font_7" style="font-size:17px">
-                         <span class="color_34">Україна, 89434, Закарпатська обл.,</span>
-                         </p>
-                         <p class="font_7" style="font-size:17px">
-                         <span class="color_34">Ужгородський р-н, село Довге Поле</span>
-                         </p>
-                         <p class="font_7" style="font-size:17px">
-                         <span class="color_34">&#65279;Email: 
-                             <object height="0">
-                             <a data-auto-recognition="true" href="mailto:karabin.uzh@gmail.com">karabin.uzh@gmail.com</a>
-                             </object>
-                         </span>
-                         </p>
-                         <p class="font_7" style="font-size:17px color:grey ">&nbsp;(*)Тут буде карта(*)</p>
-                         <hr>
-            <h6 class="font_6" style="font-size:22px">
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d9179.613521464089!2d22.32552021134186!3d48.5254220232701!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47391cf77e74bd6d%3A0x73bb9b59acf39158!2z0JTQvtC70LPQvtC1INCf0L7Qu9C1LCDQl9Cw0LrQsNGA0L_QsNGC0YHQutCw0Y8g0L7QsdC70LDRgdGC0YwsIDg5NDM0!5e1!3m2!1sru!2sua!4v1654112676036!5m2!1sru!2sua"
+              max-width="600"
+              height="450"
+              style="border: 0"
+              allowfullscreen=""
+              loading="lazy"
+              referrerpolicy="no-referrer-when-downgrade"
+            >
+            </iframe>
+            <div id="Info" class="font_6" data-testid="richTextElement">
+              <p class="font_7" style="font-size: 17px">
+                <span class="color_34">Phone1: +380503376463</span>
+              </p>
+              <p class="font_7" style="font-size: 17px">
+                <span class="color_34">Phone2: +380678891187</span>
+              </p>
+              <p class="font_7" style="font-size: 17px">
+                <span class="color_34">Україна, 89434, Закарпатська обл.,</span>
+              </p>
+              <p class="font_7" style="font-size: 17px">
+                <span class="color_34">Ужгородський р-н, село Довге Поле</span>
+              </p>
+              <p class="font_7" style="font-size: 17px">
+                <span class="color_34"
+                  >&#65279;Email:
+                  <object height="0">
+                    <a
+                      data-auto-recognition="true"
+                      href="mailto:karabin.uzh@gmail.com"
+                      >karabin.uzh@gmail.com</a
+                    >
+                  </object>
+                </span>
+              </p>
+              <p class="font_7" style="font-size:17px color:grey ">
+                &nbsp;(*)Тут буде карта(*)
+              </p>
+              <hr />
+              <h6 class="font_6" style="font-size: 22px">
                 <span class="color_14">Маєте питання чи пропозиції?</span>
-            </h6>
+              </h6>
             </div>
             <div id="comp-k4r58laj" class="_15Ac9">
-                <div data-mesh-id="comp-k4r58lajinlineContent" data-testid="inline-content" class="">
-                    <div data-mesh-id="comp-k4r58lajinlineContent-gridContainer" data-testid="mesh-container-content">
-                        <form id="comp-k4r58lbf" class="_2Jusq">
-                            <div data-mesh-id="comp-k4r58lbfinlineContent" data-testid="inline-content" class="">
-                                <div data-mesh-id="comp-k4r58lbfinlineContent-gridContainer" 
-                                data-testid="mesh-container-content">
-                                <div id="comp-k4r58lbm" class="_1FsS0 _36G3j">
-                                    <label for="input_comp-k4r58lbm" class="_1qrSw">Введіть ваше ім'я</label>
-                                     <div class="ABx0f">
-                                        <input name="введіть-ваше ім'я" id="input_comp-k4r58lbm"
-                                         class="_3rh7f has-custom-focus" type="text" placeholder="" maxlength="100"
-                                          value=""></div></div><div id="comp-k4r58lcb" class="_1FsS0 _36G3j _19QH-">
-                                    <label for="input_comp-k4r58lcb" class="_1qrSw">Введіть ваш e-mail</label>
-                                     <div class="ABx0f">
-                                        <input name="введіть-ваш e-mail" id="input_comp-k4r58lcb" class="_3rh7f has-custom-focus" type="email" placeholder="" required="" pattern="^.+@.+\.[a-zA-Z]{2,63}$" maxlength="250" value="">
-                                     </div>
-                                     </div>
-                                     <div id="comp-k4r58lcv" class="_1FsS0 _36G3j">
-                                         <label for="input_comp-k4r58lcv" class="_1qrSw">Введіть тему</label>
-                                     <div class="ABx0f">
-                                         <input name="введіть-тему" id="input_comp-k4r58lcv" class="_3rh7f has-custom-focus" type="text" placeholder="" value="">
-                                     </div>
-                                     </div> <hr>
-                                     <div id="comp-k4r58ldi" class="_1VpqH _2uzXQ">
-                                     <label for="textarea_comp-k4r58ldi" class="lW_XI">Введіть ваш відгук</label>
-                                     <br>
-                                         <textarea id="textarea_comp-k4r58ldi" class="_2Brtg has-custom-focus" 
-                                         placeholder=""></textarea></div><div id="comp-k4r58le5" aria-disabled="false" 
-                                         class="_2btH0"><button data-testid="buttonElement" class="_2k7xj">
-                                            <span class="nr31w">Надіслати</span></button>
-                                            </div>
-                                            <div id="comp-k4r58les" class="_2bafp" data-testid="richTextElement">
-                                                <p class="font_8" style="text-align:center;font-size:15px">
-                                                    <span class="color_33">Дякуємо за відгук!</span>
-                                                    </p>
-                                                    </div>
-                                                    </div></div></form></div></div></div></div></div></div></div>
- 
-  <h1>{{contacts}}</h1>
- </div>
+              <div
+                data-mesh-id="comp-k4r58lajinlineContent"
+                data-testid="inline-content"
+                class=""
+              >
+                <div
+                  data-mesh-id="comp-k4r58lajinlineContent-gridContainer"
+                  data-testid="mesh-container-content"
+                >
+                  <form id="comp-k4r58lbf" class="_2Jusq">
+                    <div
+                      data-mesh-id="comp-k4r58lbfinlineContent"
+                      data-testid="inline-content"
+                      class=""
+                    >
+                      <div
+                        data-mesh-id="comp-k4r58lbfinlineContent-gridContainer"
+                        data-testid="mesh-container-content"
+                      >
+                        <div id="comp-k4r58lbm" class="_1FsS0 _36G3j">
+                          <label for="input_comp-k4r58lbm" class="_1qrSw"
+                            >Введіть ваше ім'я</label
+                          >
+                          <div class="ABx0f">
+                            <input
+                              name="введіть-ваше ім'я"
+                              id="input_comp-k4r58lbm"
+                              class="_3rh7f has-custom-focus"
+                              type="text"
+                              placeholder=""
+                              maxlength="100"
+                              value=""
+                            />
+                          </div>
+                        </div>
+                        <div id="comp-k4r58lcb" class="_1FsS0 _36G3j _19QH-">
+                          <label for="input_comp-k4r58lcb" class="_1qrSw"
+                            >Введіть ваш e-mail</label
+                          >
+                          <div class="ABx0f">
+                            <input
+                              name="введіть-ваш e-mail"
+                              id="input_comp-k4r58lcb"
+                              class="_3rh7f has-custom-focus"
+                              type="email"
+                              placeholder=""
+                              required=""
+                              pattern="^.+@.+\.[a-zA-Z]{2,63}$"
+                              maxlength="250"
+                              value=""
+                            />
+                          </div>
+                        </div>
+                        <div id="comp-k4r58lcv" class="_1FsS0 _36G3j">
+                          <label for="input_comp-k4r58lcv" class="_1qrSw"
+                            >Введіть тему</label
+                          >
+                          <div class="ABx0f">
+                            <input
+                              name="введіть-тему"
+                              id="input_comp-k4r58lcv"
+                              class="_3rh7f has-custom-focus"
+                              type="text"
+                              placeholder=""
+                              value=""
+                            />
+                          </div>
+                        </div>
+                        <hr />
+                        <div id="comp-k4r58ldi" class="_1VpqH _2uzXQ">
+                          <label for="textarea_comp-k4r58ldi" class="lW_XI"
+                            >Введіть ваш відгук</label
+                          >
+                          <br />
+                          <textarea
+                            id="textarea_comp-k4r58ldi"
+                            class="_2Brtg has-custom-focus"
+                            placeholder=""
+                          ></textarea>
+                        </div>
+                        <div
+                          id="comp-k4r58le5"
+                          aria-disabled="false"
+                          class="_2btH0"
+                        >
+                          <button data-testid="buttonElement" class="_2k7xj">
+                            <span class="nr31w">Надіслати</span>
+                          </button>
+                        </div>
+                        <div
+                          id="comp-k4r58les"
+                          class="_2bafp"
+                          data-testid="richTextElement"
+                        >
+                          <p
+                            class="font_8"
+                            style="text-align: center; font-size: 15px"
+                          >
+                            <span class="color_33">Дякуємо за відгук!</span>
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <h1>{{ contacts }}</h1>
+  </div>
 </template>
 <script>
- export default{
-  name:'contacts',
-  data (){
-   return{
-    title:'Contacts'
-   }
-  }
- }
+export default {
+  name: "contacts",
+  data() {
+    return {
+      title: "Contacts",
+    };
+  },
+};
 </script>
 <style scoped>
- ._2egft {
-    height: auto;
-    width: 100%;
-    position: relative;
+._2egft {
+  height: auto;
+  width: 100%;
+  position: relative;
 }
-
 </style>
--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,11 +1,34 @@
-import { mount, createLocalVue } from '@vue/test-utils'
-import VueRouter from 'vue-router'
-import Contacts from "../components/contacts"
+import Home from "../../src/components/home";
+import About from "../../src/components/about_us";
+import { shallowMount, mount, createLocalVue } from "@vue/test-utils"
+import App from "@/App.vue"
+import VueRouter from "vue-router"
+import router from "@/router"
 
 const localVue = createLocalVue()
 localVue.use(VueRouter)
 
-mount(Contacts, {
-  localVue,
-  router
+describe("Home", () => {
+  it("Відрисовує домашню сторінку", async () => {
+    const wrapper = mount(App, { 
+      localVue,
+      router
+    })
+
+    router.push("/home")
+    await wrapper.vm.$nextTick()
+    
+    expect(wrapper.findComponent(Home).exists()).toBe(true)
+  });
+  it("Відрисовує 'About us'", async () => {
+    const wrapper = mount(App, { 
+      localVue,
+      router
+    })
+
+    router.push("/about_us")
+    await wrapper.vm.$nextTick()
+    
+    expect(wrapper.findComponent(About).exists()).toBe(true)
+  });
 })
